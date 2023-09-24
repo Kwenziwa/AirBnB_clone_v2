f612c3e51b65f4c4ef83f94e00f039129a70fd89
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-09-24 17:33:09 -0400

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 """This is the city class"""
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
